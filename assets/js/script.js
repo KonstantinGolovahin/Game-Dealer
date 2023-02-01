1050f4eb26082b373289dc778d8104c40c3ec0f9
unknown <sobakoved@inbox.lv>
2023-02-01 17:05:44 +0000

    Adds API calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,28 +15,60 @@ $.ajax(settings).done(function (response) {
 	console.log(response);
 });  */
 
-let keyRAWG =""
+
 // search string
-let searchText =""
-// query for RAWG API call (1 stage) - exact search PC only
-let queryURL = "https://api.rawg.io/api/games?keyRAWG&search=Halo 2&search_exact=1&exclude_additions=1,platforms=4"
+let searchText ="The Witcher 3"
+// query for RAWG API call (1 stage) - exact search PC only, 10 units max
+let apiRAWGURL = "https://api.rawg.io/api/games?key="+keyRAWG +"&search="+ searchText +"&search_exact=1&exclude_additions=1,platforms=4&page_size=10&page=1&ordering=-rating"
 // game name to be used for CheapShark API
 let resultGame =""
 
-$.ajax({
-    url: queryURL,
+ $.ajax({
+    url: apiRAWGURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function(responseRAWG) {
   
-    console.log(response)
+    console.log(responseRAWG)
 
-
-    for(i=0;i<response.results.length;i++) {
-        console.log(response.results[i].name)
-        console.log(response.results[i].background_image)
-        console.log(response.results[i].rating)
+ 
+    for(i=0;i<responseRAWG.results.length;i++) {
+        console.log(responseRAWG.results[i].name)
+        console.log(responseRAWG.results[i].background_image)
+        console.log(responseRAWG.results[i].rating)
+        // add elements to HTML + add classes from CSS
 
     }
+
+    // add click functionality to buttons to call chepshark
+
+  
+  
+  }); 
+ 
+
+resultGame = "Witcher 3";
+// lists all deals for the specific input (10 deals, but still returns more)
+  let apiCheapSharkURL= "https://www.cheapshark.com/api/1.0/deals?title="+resultGame +"&limit=10"
+
+
+  $.ajax({
+    url: apiCheapSharkURL,
+    method: "GET"
+  }).then(function(responseCheapShark) {
+  
+    console.log(responseCheapShark)
+
+ 
+     for(i=0;i<responseCheapShark.length;i++) {
+        console.log(responseCheapShark[i].title)
+        console.log(responseCheapShark[i].salePrice)
+        console.log(responseCheapShark[i].normalPrice)
+        console.log(responseCheapShark[i].isOnSale) // this should be converted to Sale/ no Sale or something similar
+        console.log(responseCheapShark[i].dealID)
+        console.log(responseCheapShark[i].storeID)  // need a list of stores???
+
+        //needs to be sorted asc by price
+    } 
 
     
 
